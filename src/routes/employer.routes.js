872255a9be99e7fb8e52ed102b788147e03cdd9b
oklenum <oliverbrinkklenum@gmail.com>
2022-10-17 15:44:51 +0200

    express.router change

---
+++ src/routes/employer.routes.js
@@ -0,0 +1,24 @@
+const { authJwt } = require("../middleware");
+const express = require("express");
+const controller = require("../controllers/employer.controller");
+const router = express.Router();
+
+router.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
+  );
+  next();
+});
+
+router.get("/test/all", controller.allAccess);
+
+router.get("/test/user", [authJwt.verifyToken], controller.userBoard);
+
+router.get(
+  "/test/admin",
+  [authJwt.verifyToken, authJwt.isAdmin],
+  controller.adminBoard
+);
+
+module.exports = router;
