cf4f2b9352fd8dae0c32dab98578e38bf667e62b
oklenum <oliverbrinkklenum@gmail.com>
2022-10-17 16:10:34 +0200

    route testing

--- src/controllers/employer.controller.js
+++ src/controllers/employer.controller.js
@@ -1,5 +1,18 @@
+const db = require("../models");
+const Employer = db.employer;
+
 exports.allAccess = (req, res) => {
   res.status(200).send("All Access");
+};
+
+exports.getEmployers = (req, res) => {
+  Employer.find()
+    .then((employers) => {
+      res.status(200).send(employers);
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
 };
 
 exports.userBoard = (req, res) => {
