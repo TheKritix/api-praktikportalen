3bf02986fb284719f24fe45be459b7d094d0a4ac
Kritix <kristoffer@kritix.dk>
2022-10-17 16:32:56 +0200

    Refactored some code

--- src/models/employer.model.js
+++ src/models/employer.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const schema = require("./schema/login.js");
+
 const Employer = mongoose.model("Employer", schema.employerSchema);
 
 module.exports = Employer;
