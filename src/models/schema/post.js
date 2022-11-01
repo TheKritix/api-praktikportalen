e374fb41ec256a7a1e0cb1b6177ee15bd97383ca
Rasmus Pedersen <rasmus.jdp@hotmail.com>
2022-11-01 16:30:35 +0100

    Additional schema values

--- src/models/schema/post.js
+++ src/models/schema/post.js
@@ -2,9 +2,13 @@ const mongoose = require("mongoose");
 
 const postsSchema = {
     title: String,
+    description: String,
     type: String,
     company: String,
-    // location: String,
+    location: String,
+    compensation: Boolean,
+    hasApplied: Boolean,
+    benefits: [String],
     // contact: String,
     // email: String,
     // phone: String,
