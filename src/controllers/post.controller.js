f74bd89f4fee8cf6a8167e0120877e3fd5b428c9
lauritzpepke <s191179@student.dtu.dk>
2022-11-01 11:47:38 +0100

    posts api

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -10,3 +10,22 @@ exports.getPosts = (req, res) => {
         res.status(500).send({ message: err.message});
     });
 }
+
+exports.createPost = (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        type: req.body.type,
+        company: req.body.company,
+        // location: req.body.location,
+        // contact: req.body.contact,
+        // email: req.body.email,
+        // phone: req.body.phone,
+    })
+    post.save((err, post) => {
+        if (err){
+            res.status(500).send({ message: err});
+            return;
+        }
+        res.send({message: "Post was succesfully created"})
+    })
+}
