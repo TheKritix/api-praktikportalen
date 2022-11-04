14eedbdbc9b372c7ac5119ea7870d8757927851b
lauritzpepke <s191179@student.dtu.dk>
2022-11-04 20:13:46 +0100

    post api

--- src/models/schema/post.js
+++ src/models/schema/post.js
@@ -4,8 +4,9 @@ const postsSchema = {
   position: String,
   type: String,
   company: String,
-  // location: String,
-  // startdate: String
+  location: String,
+  startdate: String,
+  description: String,
 };
 
 module.exports = Object.freeze({
