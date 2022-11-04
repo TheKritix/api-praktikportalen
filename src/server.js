73577b4ad892965546f237ad23b9b481a574d73e
Oliver Brink Klenum <oliverbrinkklenum@gmail.com>
2022-11-04 16:10:37 +0100

    studentlogin

--- src/server.js
+++ src/server.js
@@ -48,8 +48,8 @@ app.use(helmet());
 
 app.use(bodyParser.json());
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
 app.use(morgan("combined"));
 
