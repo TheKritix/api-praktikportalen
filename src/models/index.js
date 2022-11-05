7a353a80067202418ac5884d337de569c6b3249a
oklenum <oliverbrinkklenum@gmail.com>
2022-11-05 21:40:52 +0100

    mvp version 1.0.1 minor bug

--- src/models/index.js
+++ src/models/index.js
@@ -5,6 +5,6 @@ module.exports = Object.freeze({
   refreshToken: require("./refreshToken.model.js"),
   ROLES: ["user", "admin"],
   feedback: require("./feedback.model.js"),
-  post: require("./post.model.js"), 
+  post: require("./post.model.js"),
   gridFile: require("./gridFile.model.js"),
 });
