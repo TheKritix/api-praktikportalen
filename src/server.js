490a42eeb6c331791894c76518fbac332f90a4ec
Kristoffer <kristoffer@kritix.dk>
2022-11-28 22:30:51 +0100

    Forsøg på at lade localhost bruge vores API

--- src/server.js
+++ src/server.js
@@ -44,6 +44,17 @@ var corsOptions = {
   origin: "https://dtu.praktikportal.diplomportal.dk",
 };
 
+var whitelist = ["https://dtu.praktikportal.diplomportal.dk", "http://localhost:3001"];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 app.use(cors(corsOptions));
 
 app.use(helmet());
